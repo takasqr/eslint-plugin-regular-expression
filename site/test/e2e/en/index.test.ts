6d6e5b3a9b6f928729fbbb1caac28b860a861250
takasqr <95556025+takasqr@users.noreply.github.com>
2025-07-07 12:31:43 +0900

    trailing-slash

--- site/test/e2e/en/index.test.ts
+++ site/test/e2e/en/index.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@nuxt/test-utils/playwright'
 
 test('正しくレンダリングされることを確認', async ({ goto, page }) => {
   // ページにアクセス
-  await goto('/en', { waitUntil: 'hydration' })
+  await goto('/en/', { waitUntil: 'hydration' })
   // ヘッダーに言語切り替えのボタンが表示されるか確認
   await expect(page.getByText('日本語')).toBeVisible()
 })
--- site/test/e2e/index.test.ts
+++ site/test/e2e/index.test.ts
@@ -5,9 +5,9 @@ test('リダイレクトが正しく行われることを確認', async ({ goto,
   await goto('/', { waitUntil: 'hydration' })
 
   // ページのリダイレクトを待機
-  await page.waitForURL('**/en')
+  await page.waitForURL('**/en/')
 
   // 正しいページにリダイレクトされているか確認
   const currentUrl = new URL(page.url());
-  expect(currentUrl.pathname).toBe('/en');
+  expect(currentUrl.pathname).toBe('/en/');
 })
