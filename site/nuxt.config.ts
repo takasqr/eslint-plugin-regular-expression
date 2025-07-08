cc310ef5787ac7bd5f5d9512a89df9a65c8a9108
takasqr <95556025+takasqr@users.noreply.github.com>
2025-07-09 04:39:05 +0900

    i18n

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "@nuxt/content",
-    '@nuxtjs/i18n',
     '@nuxt/eslint'
   ],
   css: ['~/assets/css/main.css'],
@@ -12,28 +11,6 @@ export default defineNuxtConfig({
       tailwindcss: {},
       autoprefixer: {},
     },
-  },
-  i18n: {
-    locales: [
-      {
-        code: 'en',
-        iso: 'en-US'
-      },
-      {
-        code: 'ja',
-        iso: 'ja-JP'
-      },
-      {
-        code: 'es',
-        iso: 'es-ES'
-      },
-      {
-        code: 'fr',
-        iso: 'fr-FR'
-      }
-    ],
-    strategy: "no_prefix",
-    vueI18n: './i18n.config.ts'
   },
   content: {
     highlight: {
